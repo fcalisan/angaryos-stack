fd3f1f09d54161968de48eac4aae1eb78ab62b11
Ömer SAVAŞ <omer.savas@hotmail.com.tr>
2022-07-03 16:50:26 +0300

    change postgresql image

--- start.sh
+++ start.sh
@@ -13,7 +13,9 @@ chmod 777 -R ./backend/storage/logs/
 chmod 777 -R ./backend/public/
 chmod 777 -R ./backend/bootstrap/cache/
 chmod 777 -R ./services/
+
 chmod 600 ./services/postgresql/.pgpass
+chown 26:26 ./services/postgresql/.pgpass
 
 chmod 777 ./payload.sh
 chmod +x ./payload.sh
